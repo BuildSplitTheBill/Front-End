226aa76e2f2b4fd7ec939722a396d95dea930fa8
ToMMaaY <thuynh629@gmail.com>
2018-12-13 11:33:24 -0800

    Add friend functionality is working

--- split_the_bill/src/components/AddFriendForm.js
+++ split_the_bill/src/components/AddFriendForm.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 class AddFriendForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      nameInput: ""
+      name: ""
     };
   }
 
@@ -13,19 +13,16 @@ class AddFriendForm extends React.Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  addFriend = e => {
+  addFriend = (e, id) => {
     e.preventDefault();
-    this.props.addFriend(e);
-
-    this.setState({
-      nameInput: ""
-    });
+    this.props.addFriend(id);
   };
 
-  render(props) {
+  render() {
     let users = this.props.users.filter(user =>
-      user.name.includes(this.state.nameInput)
+      user.name.toLowerCase().includes(this.state.name.toLowerCase())
     );
+
     return (
       <div className="form-container">
         <h2 className="h2-header">Add New Friend</h2>
@@ -36,12 +33,16 @@ class AddFriendForm extends React.Component {
           </div>
 
           <ol>
-            {users.map(user => (
-              <li>
-                {user.name}
-                <button onClick={this.addFriend}>Add</button>
-              </li>
-            ))}
+            {this.state.name.length === 0
+              ? null
+              : users.map(user => (
+                  <li>
+                    {user.name}
+                    <button onClick={e => this.addFriend(e, user.id)}>
+                      Add
+                    </button>
+                  </li>
+                ))}
           </ol>
         </form>
       </div>
